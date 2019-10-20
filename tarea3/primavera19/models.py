46f81725c2f993c4b66d66bcaf801083df345363
RominaPazSepulveda <31572177+RominaPazSepulveda@users.noreply.github.com>
2019-10-20 12:14:21 -0300

    Arreglos tipograficos models

--- tarea3/primavera19/models.py
+++ tarea3/primavera19/models.py
@@ -3,23 +3,23 @@ from django.db import models
 # Create your models here.
 
 class Usuario(models.Model):
-	nombre=models.CharField(max_lenght=45)
-	correo=models.EmailField(max_lenght=100)
-	contrasena=models.CharField(max_lenght=45)
+	nombre=models.CharField(max_length=45)
+	correo=models.EmailField(max_length=100)
+	contrasena=models.CharField(max_length=45)
 	edad=models.IntegerField()
 
 class Testimonio(models.Model):
-	nombre=models.CharField(max_lenght=45)
-	correo=models.EmailField(max_lenght=100)
-	testimonio=models.CharField(max_lenght=512)
-	usuario=models.ForeingKey(Usuario, on_delete=models.CASCADE)
+	nombre=models.CharField(max_length=45)
+	correo=models.EmailField(max_length=100)
+	testimonio=models.CharField(max_length=512)
+	usuario=models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
 class Consulta(models.Model):
-	nombre=models.CharField(max_lenght=45)
-	detalle=models.CharField(max_lenght=512)
+	nombre=models.CharField(max_length=45)
+	detalle=models.CharField(max_length=512)
 	numero=models.IntegerField()
 	fecha=models.DateField()
-	correo=models.EmailField(max_lenght=100)
-	camion=models.CharField(max_lenght=45)
+	correo=models.EmailField(max_length=100)
+	camion=models.CharField(max_length=45)
 	foto=models.FileField(upload_to='uploads/')
-	usuario=models.ForeingKey(Usuario, on_delete=models.CASCADE)
+	usuario=models.ForeignKey(Usuario, on_delete=models.CASCADE)
